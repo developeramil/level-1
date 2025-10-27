5b937b3a10627490eedd856c419da2894581ab73
developeramil <atasovamil@gmail.com>
2025-10-27 21:37:22 +0400

    Add files via upload

---
+++ yerdeyisme kodu.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a,b,t;
+cout<<"1-ci ededi daxil edin:";
+cin>>a;
+cout<<"2-ci ededi daxil edin:";
+cin>>b;
+t=a;
+a=b;
+b=t;
+cout<<a<<endl;
+cout<<b;
+
+    return 0;
+}
