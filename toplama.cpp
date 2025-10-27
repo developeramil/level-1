6468618c3daabd8622b86b4bae835327da2b4207
developeramil <atasovamil@gmail.com>
2025-10-27 21:37:36 +0400

    Add files via upload

---
+++ toplama.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+   int a,b;
+   cout<<" a ededini daxil edin :";
+   cin>> a;
+   cout<<"b ededini daxil edin :";
+   cin>> b;
+    int hasil = a*b;
+    cout<< hasil;
+    return 0;
+}
