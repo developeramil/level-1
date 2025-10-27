3c0e4abb1b7e409413059d917a5ff3bc1aa7a7d5
developeramil <atasovamil@gmail.com>
2025-10-27 21:37:09 +0400

    Add files via upload

---
+++ ededi-orta.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a,b,c;
+cout<<"1-ci ededi daxil edin:";
+cin>>a;
+cout<<"2-ci ededi daxil edin:";
+cin>>b;
+cout<<"3-ci ededi daxil edin:";
+cin>>c;
+int EO=(a+b+c)/3;
+cout<<EO;
+    return 0;
+}
