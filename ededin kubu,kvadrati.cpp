600acf29b2f50c5f5e6afb6583eb4c918218f6e3
developeramil <atasovamil@gmail.com>
2025-10-27 21:36:54 +0400

    Add files via upload

---
+++ ededin kubu,kvadrati.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int eded;
+ cout<< "eded daxil edin :";
+ cin>>eded;
+ int ededKvadrati = eded*eded;
+ cout<<"ededKvadrati:"<<ededKvadrati<<endl;
+ int ededKUbu = eded*eded*eded;
+ cout<<"ededinkubu:"<<ededKUbu;
+ return 0;
+}
