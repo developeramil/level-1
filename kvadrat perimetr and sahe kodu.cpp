de42c1dc38fb028d1c586944a2c53c9b24b7dfd5
developeramil <atasovamil@gmail.com>
2025-10-27 21:37:54 +0400

    Add files via upload

---
+++ kvadrat perimetr and sahe kodu.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int kvadratteref;
+cout<<"kvadratin terefini daxil edin:";
+cin>>kvadratteref;
+int sahe=kvadratteref*kvadratteref;
+int perimetr=kvadratteref*4;
+cout<<"kvadratin perimetri:"<<perimetr<<endl;
+cout<<"kvadratin sahesi:"<<sahe<<endl;
+    return 0;
+}
