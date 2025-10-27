b977fa0a7adb25660cc2c6875b73cd9acb3058ca
developeramil <atasovamil@gmail.com>
2025-10-27 21:51:52 +0400

    Add files via upload

---
+++ Faktorial.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ using namespace std;
+ int main (){
+    int c,b,a;
+a=0;
+c=1;
+cout<<"ededi daxil edin:";
+cin>>b;
+while(a<b){
+a++;
+c=c*a;
+
+}
+cout<<c;
+
+    return 0;
+ }
