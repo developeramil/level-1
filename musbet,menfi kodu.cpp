ecbbe475a8fbc6d67e619c7d4a337adcfe52a797
developeramil <atasovamil@gmail.com>
2025-10-27 21:45:46 +0400

    Add files via upload

---
+++ musbet,menfi kodu.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a;
+cout<<"eded daxil edin:";
+cin>>a;
+ if (a>0){cout<<"eded musbetdir";}
+ else if(a<0){cout<<"eded menfidir";}
+else{cout<<"eded sifra beraberdir";}
+    return 0;
+}
