110419caa8061ab50b2d2cb256975795a30465ae
developeramil <atasovamil@gmail.com>
2025-10-27 21:36:37 +0400

    Add files via upload

---
+++ daire kodu.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int radius;
+ float pi= 3.14;
+cout<<"radius daxil edin:";
+cin>>radius;
+int b= radius*radius*pi;
+cout<<b;
+
+
+    return 0;
+}
