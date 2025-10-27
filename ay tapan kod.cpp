ce1ae94812fd259b60bd54d220673aa16baf97a6
developeramil <atasovamil@gmail.com>
2025-10-27 21:45:19 +0400

    Add files via upload

---
+++ ay tapan kod.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a;
+cout<<"ayi daxil edin";
+cin>>a;
+if(a==1){cout<<"daxil etdiyiniz ay yanvardir";}
+else if(a==2){cout<<"daxil etdiyinz ay fevraldir";}
+else if(a==3){cout<<"daxil etdiyiniz ay martdir";}
+else if (a==4){cout<<"daxil etdiyiniz ay apreldir";}
+else if(a==5){cout<<"daxil etdiniz ay maydir";}
+else if(a==6){cout<<"daxil etdiyiniz ay iyunur";}
+else if(a==7){cout<<"daxil etdiyiniz ay iyuldur";}
+else if(a==8){cout<<"daxil etdiyiniz ay avqustdur";}
+else if(a==9){cout<<"daxil etdiyiniz ay sentyabrdir";}
+else if(a==10){cout<<"daxil etdiyiniz ay oktyabirdir";}
+else if(a==11){cout<<"daxil etdiyiniz ay noyabrdir";}
+else{cout<<"daxil etdiyiniz ay dekabrdir";}
+    return 0;
+}
