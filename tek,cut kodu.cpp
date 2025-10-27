22f0e6a925ba223723056539553dfcdd699f0c08
developeramil <atasovamil@gmail.com>
2025-10-27 21:46:28 +0400

    Add files via upload

---
+++ tek,cut kodu.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a;
+cout<<"eded daxil edin:";
+cin>>a;
+if(a%2==1){cout<<"eded tekdir";}
+else if(a==0){cout<<"eded sifra beraberdir";}
+else{cout<<"eded cutdur";}
+    return 0;
+}
