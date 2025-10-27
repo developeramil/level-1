e4bd08c936226a8699692c81a8c098de2e87de7a
developeramil <atasovamil@gmail.com>
2025-10-27 21:45:33 +0400

    Add files via upload

---
+++ ededlerden boyuk olani tapan kod.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a,b;
+cout<<"1-ci ededi daxil edin";
+cin>>a;
+cout<<"2-ci daxil edin:";
+cin>>b;
+if(a>b){cout<<"boyuk olan eded 1-ci dir:"<<a;}
+else{cout<<"boyuk olan eded 2-ci dir:"<<b;}
+    return 0;
+}
